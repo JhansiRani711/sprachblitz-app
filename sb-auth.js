2a2c78c333bc7cf2ac1efff7b9e6561c58a96681
JhansiRani711 <100845035+JhansiRani711@users.noreply.github.com>
2026-08-12 09:37:10 +0200

    Cleanup unused Firebase SDK imports
    Removed unused Firebase imports and related comments.

--- sb-auth.js
+++ sb-auth.js
@@ -16,11 +16,6 @@
 //   Apple sign-in additionally needs a paid Apple Developer account.
 //   Leave ENABLE_APPLE false until you have one.
 // =====================================================================
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
