d1a34584954a5c5744da67a12e5d521aa6eb3d47
Jhansi Rani Muthyala <100845035+Sprachblitz-app@users.noreply.github.com>
2026-08-20 17:13:13 +0200

    Update sb-auth.js

--- sb-auth.js
+++ sb-auth.js
@@ -27,7 +27,6 @@ const db = getFirestore(app);
 
 export { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInAnonymously, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut };
 
-// Tell index.html that Firebase is ready!
 export function sbAuthReady() {
     return true;
 }
