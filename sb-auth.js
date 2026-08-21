4a78f5da2aa140433037e2c654b433a5349e72f5
Jhansi Rani Muthyala <100845035+Sprachblitz-app@users.noreply.github.com>
2026-08-21 02:57:21 +0200

    Change Firebase API key
    Updated Firebase API key in sb-auth.js.

--- sb-auth.js
+++ sb-auth.js
@@ -13,7 +13,7 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase
 
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAMk_LK3HqGX0B5wjo75hrMC71wzhxat0Q",
+    apiKey: "AIzaSyDIgW7mv8iA8ap9IATuKZjEmZoF60Cd6vA",
     authDomain: "sprachblitz.firebaseapp.com",
     projectId: "sprachblitz",
     storageBucket: "sprachblitz.firebasestorage.app",
